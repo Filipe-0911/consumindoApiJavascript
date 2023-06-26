51b5ca385c6b57f07bfeee05f246831c4161ec56
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-06-26 13:47:41 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log(`mandando oi pro amigo`);
+
+function mandaMensagem () {
+    console.log(`tudo bem?`);
+    console.log(`vou te mandar uma solicitacao`);
+    console.log(`solicitacao recebida`)
+
+}
+
+mandaMensagem();
+
+console.log(`tchau`)
