d7d402b1c61b3b75d01e0a0d1059481da266bf52
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-07-22 17:41:03 -0300

    finalizando projeto CEP

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-var cep = prompt('Digite um CEP');
+var endereco = document.getElementById(`endereco`);
+var bairro = document.getElementById(`bairro`);
+var cidade = document.getElementById(`cidade`);
+var estado = document.getElementById(`estado`);
+var cep = document.getElementById('cep');
 
-var consultaCep = fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro) {
-            throw Error('Esse CEP não existe!')
-        } else {
-            console.log(r)
+async function buscaEndereco(cep) {
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+
+        if (consultaCepConvertida.erro) {
+            throw Error(`CEP não existente!`)
         }
-        })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído'));
+        //console.log(consultaCepConvertida);
+        return insereDadosHtml(consultaCepConvertida);
+
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
+//focusout é quando vc 'des-seleciona'
+cep.addEventListener(`focusout`, () => buscaEndereco(cep.value));
+
+function insereDadosHtml(enderecoCompleto) {
+    endereco.value = enderecoCompleto.logradouro;
+    bairro.value = enderecoCompleto.bairro;
+    cidade.value = enderecoCompleto.localidade;
+    estado.value = enderecoCompleto.uf;
+}
