a81bf14457167fed2651c2281250c95d9cdbbdeb
Filipe <fbianchi.andrade@gmail.com>
2023-07-24 08:37:57 -0300

    finalizando projeto

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-var pegaCep = document.getElementById('cep');
-var cep = `71060639`;
+var endereco = document.getElementById(`endereco`);
+var bairro = document.getElementById(`bairro`);
+var cidade = document.getElementById(`cidade`);
+var estado = document.getElementById(`estado`);
+var cep = document.getElementById('cep');
+var mensagemErro = document.getElementById(`erro`);
 
-    var consultaCep = fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(resposta => resposta.json())
-    .then(r => {
-        //utilizado r.erro pois quando informado um cep de formato valido ele retorna um erro = true
-        if(r.erro) { 
-            throw Error('Esse CEP não existe!')
-        } else {
-            console.log(r)
+async function buscaEndereco(cep) {
+    mensagemErro.innerHTML = "";
+    var cepModificar = document.getElementById('cep');
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+
+        if (consultaCepConvertida.erro) {
+            throw Error(`CEP não existente!`)
         }
-        })
-    .catch(erro => {
-        if(erro) {
-            throw Error('Cep inválido!')
-        } 
-    })
-    .finally(mensagem => console.log('Processamento concluído'));
+        cepModificar.className = `campo__escrita`;
+        return insereDadosHtml(consultaCepConvertida);
 
+    } catch (erro) {
+        cepModificar.className = `campo__escrita2`;
+        mensagemErro.innerHTML = `<span class="erro__imagem">
+        </span><p class="erro__texto">CEP inválido. Tente novamente!</p>`;
+        
+    }
+}
+
+cep.addEventListener(`focusout`, () => buscaEndereco(cep.value));
+
+function insereDadosHtml(enderecoCompleto) {
+    endereco.value = enderecoCompleto.logradouro;
+    bairro.value = enderecoCompleto.bairro;
+    cidade.value = enderecoCompleto.localidade;
+    estado.value = enderecoCompleto.uf;
+}
